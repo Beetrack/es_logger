369dcac6ef49addad0d1e496729629e43b368e7e
Andres Colonia <andreskal@gmail.com>
2020-04-01 13:47:55 -0300

    feat: refactoring test

--- test/application_test.rb
+++ test/application_test.rb
@@ -4,14 +4,23 @@ class RackESLoggerAppTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
-    app = lambda { |env| [200, { 'Content-Type' => 'text/html' }, ['ok']] }
-    RackESLogger::Application.new(app)
+    app = ->(_env) { [200, { 'Content-Type' => 'text/html' }, ['ok']] }
+    @rack_app = RackESLogger::Application.new(app)
   end
 
   def test_get_request
-    get '/api/external/v1/1', name: 'name', lastname: 'lastname'
+    url = '/api/external/v1/1'
+    params = { name: 'name', lastname: 'lastname' }
+    get url, params
 
+    puts @rack_app.payload
+
+    default_assert
+  end
+
+  def default_assert
     assert last_response.ok?
+    assert_equal last_response.body, 'ok'
     assert_equal last_response.headers['Content-Type'], 'text/html'
   end
 end
