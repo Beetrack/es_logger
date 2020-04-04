71b763f09de209ed4a405b7a1d896f332fe7e38e
Andres Colonia <andreskal@gmail.com>
2020-04-04 11:36:09 -0300

    feat: include elasticsearch client configuration

--- lib/generators/rack_es_logger/templates/initializer.rb
+++ lib/generators/rack_es_logger/templates/initializer.rb
@@ -4,4 +4,13 @@ RackEsLogger.configure do |config|
   # config.use_sidekiq
   # You can supply custom Sidekiq options:
   # config.use_sidekiq 'queue' => 'low'
+  # Configure elasticsearh client credentials
+  # Config.elasticsearch = {
+  #   user: '',
+  #   password: '',
+  #   host: '',
+  #   port: 9200
+  # }
 end
+
+RackEsLogger::Elasticsearch::Client.instance.register_credentials
