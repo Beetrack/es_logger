ccb77dc1034e51297784f2223b1ffbe953bc46c6
Andres Colonia <andreskal@gmail.com>
2020-04-04 22:47:39 -0300

    feat:include index_name param

--- lib/generators/rack_es_logger/templates/initializer.rb
+++ lib/generators/rack_es_logger/templates/initializer.rb
@@ -5,12 +5,15 @@ RackEsLogger.configure do |config|
   # You can supply custom Sidekiq options:
   # config.use_sidekiq 'queue' => 'low'
   #
+  # params configuration to connect with Elasticsearch
   config.elasticsearch = {
     user: ENV['ELASTICSEARCH_USER'],
     password: ENV['ELASTICSEARCH_PASSWORD'],
     host: ENV['ELASTICSEARCH_HOST'],
     port: ENV['ELASTICSEARCH_PORT']
   }
+
+  config.elasticsearch_index_name = ENV['ELASTICSEARCH_INDEX_NAME']
 
   config.elasticsearch_pool_size = ENV['ELASTICSEARCH_POOL_SIZE'] || 10
   config.elasticsearch_timeout = ENV['ELASTICSEARCH_TIMEOUT'] || 5
