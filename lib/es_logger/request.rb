6f335646ca38099a3e27d021350a06c51793d0bb
Andres Colonia <andreskal@gmail.com>
2020-04-16 14:04:35 -0400

    feat: create request class and separate from rack

---
+++ lib/es_logger/request.rb
@@ -0,0 +1,20 @@
+require 'es_logger/configuration'
+require 'es_logger/elasticsearch/client_connection_pool'
+
+module EsLogger
+  class Request
+    def call(response)
+      response['timestamp'] = Time.now.utc
+
+      client.index index: EsLogger.configuration.elasticsearch_index_name, body: response
+    rescue ::Elasticsearch::Transport::Transport::Errors::ServiceUnavailable
+      puts 'Cannot connect with Elastisearch service'
+    end
+
+    private
+
+    def client
+      ::EsLogger::Elasticsearch::ClientConnectionPool.instance.client.with { |client| client }
+    end
+  end
+end
