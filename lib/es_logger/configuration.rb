5f81c262fb41eb28741988c7d82841f3db73b4b7
Andres Colonia <andreskal@gmail.com>
2020-04-16 14:03:21 -0400

    feat: support to sidekiq process

--- lib/es_logger/configuration.rb
+++ lib/es_logger/configuration.rb
@@ -9,16 +9,23 @@ module EsLogger
   end
 
   class Configuration
+    attr_accessor :async_handler
     attr_accessor :include_pattern
     attr_accessor :elasticsearch
     attr_accessor :elasticsearch_index_name
     attr_accessor :elasticsearch_pool_connection
 
     def initialize
+      @async_handler = nil
       @include_pattern = nil
       @elasticsearch = { host: 'localhost', port: 9200, user: 'elastic', password: nil, log: true }
       @elasticsearch_index_name = 'request'
       @elasticsearch_pool_connection = { size: 10, timeout: 5 }
+    end
+
+    def use_sidekiq(options = {})
+      require 'es_logger/delay/sidekiq'
+      @async_handler = EsLogger::Delay::Sidekiq.new(options)
     end
   end
 end
---
+++ lib/es_logger/delay/sidekiq.rb
@@ -0,0 +1,24 @@
+require 'sidekiq'
+
+module EsLogger
+  module Delay
+    class Sidekiq
+
+      OPTIONS = { 'queue' => 'es_logger', 'class' => EsLogger::Delay::Sidekiq }.freeze
+
+      def initialize(*args)
+        @options = (opts = args.shift) ? OPTIONS.merge(opts) : OPTIONS
+      end
+
+      def call(payload)
+        raise StandardError, 'Unable to push the job to Sidekiq' if ::Sidekiq::Client.push(@options.merge('args' => [payload])).nil?
+      end
+
+      include ::Sidekiq::Worker
+
+      def perform(args)
+        EsLogger::Request.new.call(args)
+      end
+    end
+  end
+end
