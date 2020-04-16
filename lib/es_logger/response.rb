d1161fd525798e7450c8acd283caf121b6ef8dc8
Andres Colonia <andreskal@gmail.com>
2020-04-16 17:04:24 -0400

    refactor: use class method to process response

--- lib/es_logger/response.rb
+++ lib/es_logger/response.rb
@@ -1,30 +1,24 @@
 module EsLogger
   class Response
-    def initialize(env)
-      @env = env
-    end
-
-    def call
+    def self.call(env)
       payload = {
-        'remote_address' => @env['REMOTE_ADDR'],
-        'request_method' => @env['REQUEST_METHOD'],
-        'path' => @env['PATH_INFO'],
-        'query_string_params' => ::Rack::Utils.parse_nested_query(@env['QUERY_STRING']),
-        'params' => ::Rack::Utils.parse_query(@env['rack.input'].read, '&')
+        'remote_address' => env['REMOTE_ADDR'],
+        'request_method' => env['REQUEST_METHOD'],
+        'path' => env['PATH_INFO'],
+        'query_string_params' => ::Rack::Utils.parse_nested_query(env['QUERY_STRING']),
+        'params' => ::Rack::Utils.parse_query(env['rack.input'].read, '&')
       }
 
-      payload.merge!(controller_metrics(@env['action_controller.instance']))
+      controller = env['action_controller.instance']
+
+      if controller
+        payload.merge!(
+          'controller_name' => controller.controller_name,
+          'controller_action' => controller.action_name,
+          'content_type' => controller.content_type || :none
+        )
+      end
       payload
-    end
-
-    def controller_metrics(controller)
-      return {} if controller.nil?
-
-      {
-        'controller_name' => controller.controller_name,
-        'controller_action' => controller.action_name,
-        'content_type' => controller.content_type || :none
-      }
     end
   end
 end
